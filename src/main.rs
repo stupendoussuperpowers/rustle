699a5e139b37032a4bbe9a6545ebe6ac8974f1bb
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2024-12-01 15:07:12 -0500

    getopt; interface; file; savefile

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use pcap::{Device, PacketHeader};
+use pcap::{Activated, Capture, Device, Offline, Packet, PacketHeader};
 use pktparse::{
     ethernet::{parse_ethernet_frame, EtherType, EthernetFrame},
     ipv4::parse_ipv4_header,
@@ -7,13 +7,15 @@ use pktparse::{
     udp::{parse_udp_header, UdpHeader},
 };
 
+use getopt::Opt;
+
 // use pktparse::ip::IPProtocol::{TCP, UDP};
 use pktparse::ipv4::IPv4Header;
 use pktparse::ipv6::IPv6Header;
 
 use pktparse::tcp::TcpHeader;
 
-use std::env;
+use std::{env, path::Path, process::exit};
 
 #[derive(Debug)]
 enum IPlayer {
@@ -64,75 +66,128 @@ impl CapturedFrame {
     }
 }
 
+fn read_capture_file<P>(path: P) -> Capture<Offline>
+where
+    P: AsRef<Path>,
+{
+    Capture::from_file(path).unwrap()
+}
+
+fn process_packet(packet: Packet) {
+    let (remaining, ethernet_frame) = parse_ethernet_frame(&packet.data).unwrap();
+
+    let (remaining, internet_frame) = match ethernet_frame.ethertype {
+        EtherType::IPv4 => {
+            let (a, b) = parse_ipv4_header(&remaining).unwrap();
+            (Some(a), Some(IPlayer::V4(b)))
+        }
+        EtherType::IPv6 => {
+            let (a, b) = parse_ipv6_header(&remaining).unwrap();
+            (Some(a), Some(IPlayer::V6(b)))
+        }
+        _ => (None, None),
+    };
+
+    if internet_frame.is_none() {
+        return;
+    }
+
+    let internet_frame_val = internet_frame.unwrap();
+
+    let app_protocol = match internet_frame_val {
+        IPlayer::V4(iframe) => iframe.protocol,
+        IPlayer::V6(iframe) => iframe.next_header,
+    };
+
+    let app_frame = match app_protocol {
+        pktparse::ip::IPProtocol::TCP => {
+            let (_a, b) = parse_tcp_header(&remaining.unwrap()).unwrap();
+            Some(AppLayer::TCP(b))
+        }
+        pktparse::ip::IPProtocol::UDP => {
+            let (_a, b) = parse_udp_header(&remaining.unwrap()).unwrap();
+            Some(AppLayer::UDP(b))
+        }
+        _ => None,
+    };
+
+    if app_frame.is_none() {
+        return;
+    }
+
+    let cap_frame = CapturedFrame {
+        packet_header: *packet.header,
+        ethernet: Some(ethernet_frame),
+        internet: Some(internet_frame_val),
+        application: Some(app_frame.unwrap()),
+    };
+
+    cap_frame.print_status_line();
+}
+
+struct PcapOptions {
+    file: Option<String>,
+    interface: Option<String>,
+    savefile: Option<String>,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let interface = &args[1];
+    let mut opts = getopt::Parser::new(&args, "f:i:o:");
 
-    let dev_list = Device::list().unwrap();
+    let mut capt: Capture<dyn Activated>;
 
-    let dev = dev_list.iter().find(|&dev| dev.name == *interface).unwrap();
+    let mut pcap_options: PcapOptions = PcapOptions {
+        file: None,
+        interface: None,
+        savefile: None,
+    };
 
-    let mut capture = dev.clone().open().unwrap();
+    loop {
+        match opts.next().transpose().unwrap() {
+            None => {
+                break;
+            }
+            Some(opt) => match opt {
+                Opt('f', Some(arg)) => pcap_options.file = Some(arg),
+                Opt('i', Some(arg)) => pcap_options.interface = Some(arg),
+                Opt('o', Some(arg)) => pcap_options.savefile = Some(arg),
+                _ => unreachable!(),
+            },
+        };
+    }
 
-    let savefile = if args.len() > 2 {
-        let savefile_mut = capture.savefile(&args[2]).unwrap();
+    if pcap_options.file.is_none() && pcap_options.interface.is_none() {
+        println!("Help string");
+        exit(0);
+    }
+
+    if pcap_options.file.is_some() {
+        capt = read_capture_file(pcap_options.file.unwrap()).into();
+    } else {
+        let dev_list = Device::list().unwrap();
+
+        let dev = dev_list
+            .iter()
+            .find(|&dev| dev.name == pcap_options.interface.clone().unwrap())
+            .unwrap();
+
+        capt = dev.clone().open().unwrap().into()
+    }
+
+    while let Ok(packet) = capt.next_packet() {
+        process_packet(packet);
+    }
+
+    let savefile = if pcap_options.savefile.is_some() {
+        let savefile_mut = capt.savefile(pcap_options.savefile.unwrap()).unwrap();
         &mut Some(savefile_mut)
     } else {
         &mut None
     };
 
-    while let Ok(packet) = capture.next_packet() {
-        let (remaining, ethernet_frame) = parse_ethernet_frame(&packet.data).unwrap();
-
-        let (remaining, internet_frame) = match ethernet_frame.ethertype {
-            EtherType::IPv4 => {
-                let (a, b) = parse_ipv4_header(&remaining).unwrap();
-                (Some(a), Some(IPlayer::V4(b)))
-            }
-            EtherType::IPv6 => {
-                let (a, b) = parse_ipv6_header(&remaining).unwrap();
-                (Some(a), Some(IPlayer::V6(b)))
-            }
-            _ => (None, None),
-        };
-
-        if internet_frame.is_none() {
-            continue;
-        }
-
-        let internet_frame_val = internet_frame.unwrap();
-
-        let app_protocol = match internet_frame_val {
-            IPlayer::V4(iframe) => iframe.protocol,
-            IPlayer::V6(iframe) => iframe.next_header,
-        };
-
-        let app_frame = match app_protocol {
-            pktparse::ip::IPProtocol::TCP => {
-                let (_a, b) = parse_tcp_header(&remaining.unwrap()).unwrap();
-                Some(AppLayer::TCP(b))
-            }
-            pktparse::ip::IPProtocol::UDP => {
-                let (_a, b) = parse_udp_header(&remaining.unwrap()).unwrap();
-                Some(AppLayer::UDP(b))
-            }
-            _ => None,
-        };
-
-        if app_frame.is_none() {
-            continue;
-        }
-
-        let cap_frame = CapturedFrame {
-            packet_header: *packet.header,
-            ethernet: Some(ethernet_frame),
-            internet: Some(internet_frame_val),
-            application: Some(app_frame.unwrap()),
-        };
-
-        cap_frame.print_status_line();
-
+    while let Ok(packet) = capt.next_packet() {
         match savefile {
             Some(file) => file.write(&packet),
             None => (),
